c11f9d7bfe9db9fb6028576bf963e7daceac67cb
Fuz Home <fuzhome98@gmail.com>
2025-09-04 09:07:50 +0000

    no it is not resolved. pls correct it

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+import '../globals.css';
+import { Toaster } from "@/components/ui/toaster";
+import { Inter } from 'next/font/google';
+import { PageLoader } from '@/components/layout/page-loader';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'BotPilot',
+  description: 'Host your crypto bots without any VPS or complex setups.',
+};
+
+export default function MarketingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark" suppressHydrationWarning>
+      <body className={inter.className}>
+        <PageLoader />
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
