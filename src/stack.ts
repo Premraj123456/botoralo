66cdb59911dd08734a36f7df995d312a9bbf210e
Fuz Home <fuzhome98@gmail.com>
2025-09-05 15:11:50 +0000

    Try fixing this error: `Runtime Error: TypeError: _stackframe_stack__WEB

--- src/stack.ts
+++ src/stack.ts
@@ -1,6 +1,6 @@
-import { Stack } from '@stackframe/stack';
+import * as StackModule from '@stackframe/stack';
 
-export const stack = new Stack({
+export const stack = new StackModule.Stack({
   project: 'projectnine',
   // an empty object means use default settings
   // for all environments
