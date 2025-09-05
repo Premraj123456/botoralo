d8f6e5b251b3bec0a4a6c75393c7170dd6d9fe6d
Fuz Home <fuzhome98@gmail.com>
2025-09-05 15:43:59 +0000

    Try fixing this error: `Runtime Error: TypeError: (0 , react__WEBPACK_IM

--- src/lib/stytch/auth.ts
+++ src/lib/stytch/auth.ts
@@ -1,14 +1,15 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { stytchClient } from './server';
-import { getStytchSession } from '@stytch/nextjs';
+import { loadStytchClient } from './server';
+import { getStytchSession } from '@stytch/nextjs/server';
 
 export const getCurrentUser = async () => {
     return getStytchSession();
 };
 
 export const stytchLogout = async () => {
+    const stytchClient = loadStytchClient();
     // The session token is read from cookies automatically by the SDK
     return stytchClient.sessions.revoke({
         session_token: cookies().get('stytch_session')?.value
--- src/lib/stytch/server.ts
+++ src/lib/stytch/server.ts
@@ -1,10 +1,21 @@
-import { loadStytch } from '@stytch/nextjs';
+import { loadStytch } from '@stytch/nextjs/server';
 import * as stytch from 'stytch';
 
-let client: stytch.Client;
+let stytchClient: stytch.Client;
 
-const stytchEnv = stytch.envs.test;
+const loadStytchClient = () => {
+  if (!stytchClient) {
+    const stytchEnv = process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN!.startsWith('public-token-test')
+      ? stytch.envs.test
+      : stytch.envs.live;
+    
+    stytchClient = new stytch.Client({
+      project_id: process.env.NEXT_PUBLIC_STYTCH_PROJECT_ID!,
+      secret: process.env.STYTCH_SECRET!,
+      env: stytchEnv,
+    });
+  }
+  return stytchClient;
+};
 
-const stytchClient = loadStytch();
-
-export { stytchClient };
+export { loadStytchClient };
