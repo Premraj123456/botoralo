3f4ca27c18e1036275f3d6aa3109999791051144
Fuz Home <fuzhome98@gmail.com>
2025-09-05 13:43:49 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`${inter.variable} ${sourceCodePro.variable}`}>
+    <html lang="en" className="dark">
+      <body className={`${inter.variable} ${sourceCodePro.variable}`} suppressHydrationWarning>
         <PageLoader />
         {children}
         <Toaster />
