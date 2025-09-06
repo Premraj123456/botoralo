62a7d633739e5804bd6cb468a6f1a7db7451d254
Fuz Home <fuzhome98@gmail.com>
2025-09-06 04:36:54 +0000

    Try fixing this error: `Console Error: [Stytch] "This application is usi

--- src/lib/stytch/StytchProvider.tsx
+++ src/lib/stytch/StytchProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { StytchProvider as StytchProviderBase } from '@stytch/nextjs';
-import { createStytchUIClient } from '@stytch/nextjs/ui';
+import { createStytchB2BUIClient } from '@stytch/nextjs/b2b/ui';
 
-const stytch = createStytchUIClient(process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN!);
+const stytch = createStytchB2BUIClient(process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN!);
 
 export function StytchProvider({ children }: { children: React.ReactNode }) {
   return <StytchProviderBase stytch={stytch}>{children}</StytchProviderBase>;
