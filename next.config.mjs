01a19a3de8398e0340508eff0aa04ba73dc4fc54
Fuz Home <fuzhome98@gmail.com>
2025-09-10 06:27:39 +0000

    ✓ Starting...  ⚠ Invalid next.config.mjs options detected:  ⚠     Unrec

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-      "https://3000-firebase-studio-1756971242442.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev",
-    ],
+  async rewrites() {
+    return [
+      {
+        source: '/_next/:path*',
+        destination:
+          'http://3000-firebase-studio-1756971242442.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev/_next/:path*',
+      },
+    ];
   },
 };
 
