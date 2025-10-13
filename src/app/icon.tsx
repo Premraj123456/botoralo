48bcee7d2aa56adee7a9d438b824557f4c40d264
Fuz Home <fuzhome98@gmail.com>
2025-10-13 15:03:11 +0000

    make it little bit large

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -26,8 +26,8 @@ export default function Icon() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="28"
-          height="28"
+          width="30"
+          height="30"
           viewBox="0 0 24 24"
           fill="none"
           stroke="#8A2BE2" // Primary brand color
